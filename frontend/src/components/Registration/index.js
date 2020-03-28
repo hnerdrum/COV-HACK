438685cb1b9cea3efe84c420d4c863e0c29a2799
Henrik Nerdrum <henrik.nerdrum@gmail.com>
2020-03-28 13:25:02 +0100

    Add initial routing

---
+++ frontend/src/components/Registration/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h1>Registration</h1>
+            <p>Registration page body content</p>
+        </div>
+    );
+};
+
+export default Registration;
