13d60cc0d95815873fb871692e5951ad7cccb0a6
sindretoftnordal <sindretn@stud.ntnu.no>
2020-03-29 23:56:01 +0200

    Map dynamics

--- frontend/src/components/Home/MapComponent.js
+++ frontend/src/components/Home/MapComponent.js
@@ -3,47 +3,72 @@ import L from 'leaflet';
 
 const MapComponent = ({ db }) => {
 
-  const [faceMaskCount, setFaceMaskCount] = useState(0);
+  const [mymapRef, setMap] = useState({});
+  const [mapIsSet, setMapIsSet] = useState(false);
   const [hospitals, setHospitals] = useState([]);
+  const [dataIsLoaded, setLoaded] = useState(false);
 
   useEffect(() => {
     loadData(db)
-    console.log(hospitals)
     loadMap()
-  }, [""])
+  }, [dataIsLoaded])
+
+
 
   const loadData = () => {
-    return db.collection("hospitals")
-      .get()
-      .then((snap) => {
-        snap.forEach((doc) => {
-          setHospitals(hospitals => [...hospitals, {...doc.data()}])
+    if(!dataIsLoaded){
+      console.log("holalala")
+      return db.collection("hospitals")
+        .get()
+        .then((snap) => {
+          snap.forEach((doc) => {
+            setHospitals(hospitals => [...hospitals, {...doc.data()}])
+            })
+          setLoaded(true)
         })
-      })
+    }
   };
 
   const loadMap = () => {
-    const myMap = L.map('mapid', {
-      center: [51.454527, -2.587910],
-      zoom: 11,
-      layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
-      ]
+    if (!mapIsSet){
+      console.log("tittei")
+      const mymap = L.map('mapid', {
+        center: [54.070192, -3.869140],
+        zoom: 5,
+        layers: [
+          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          }),
+        ]
+      })
+      setMap(mymap)
+      setMapIsSet(true)
+    }
+
+    hospitals.map((h) => {
+      var popup6 = L.circleMarker([h.lat, h.lng], { radius: 10}).addTo(mymapRef);
+      var inventoryInfo = "";
+      h.equipment.map((x) => {
+        var category = x.category;
+        var available = x.available;
+        var inuse = x.inUse;
+        var reserved = x.reserved
+        inventoryInfo = inventoryInfo + category + "<br>" + "Available: " + available + "<br>" + "In use: " + inuse +"<br>"+ "Reserved: " + reserved +"<br><br>";
+      });
+      popup6.bindPopup(inventoryInfo);
     })
 
-    var popup1 = L.circleMarker([51.497619, -2.590936], { radius: 15}).addTo(myMap);
-    var popup2 = L.circleMarker([51.468747, -2.516716], { radius: 15}).addTo(myMap);
-    var popup3 = L.circleMarker([51.412819, -2.583208], { radius: 15}).addTo(myMap);
-    var popup4 = L.circleMarker([51.457448, -2.622750], { radius: 15}).addTo(myMap);
-    var popup5 = L.circleMarker([51.458354, -2.593978], { radius: 15}).addTo(myMap);
+    //var popup1 = L.circleMarker([51.497619, -2.590936], { radius: 15}).addTo(map);
+    //var popup2 = L.circleMarker([51.468747, -2.516716], { radius: 15}).addTo(map);
+    //var popup3 = L.circleMarker([51.412819, -2.583208], { radius: 15}).addTo(map);
+    //var popup4 = L.circleMarker([51.457448, -2.622750], { radius: 15}).addTo(map);
+    //var popup5 = L.circleMarker([51.458354, -2.593978], { radius: 15}).addTo(map);
 
-    popup1.bindPopup("<b>Hello world!</b><br>I am a popup.")
-    popup2.bindPopup("<b>Hello world!</b><br>I am a popup.")
-    popup3.bindPopup("<b>Hello world!</b><br>I am a popup.")
-    popup4.bindPopup("<b>Hello world!</b><br>I am a popup.")
-    popup5.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    //popup1.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    //popup2.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    //popup3.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    //popup4.bindPopup("<b>Hello world!</b><br>I am a popup.")
+    //popup5.bindPopup("<b>Hello world!</b><br>I am a popup.")
   };
 
   // 51.497619, -2.590936
